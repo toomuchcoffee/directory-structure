1cdb4007b60e3fc4cf11510e8820b0155a535b8b
toomuchcoffee <6577546+toomuchcoffee@users.noreply.github.com>
2025-09-09 23:30:07 +0200

    refactors test

--- src/test/scala/ModelExtensionsTest.scala
+++ src/test/scala/ModelExtensionsTest.scala
@@ -10,80 +10,75 @@ import java.nio.file.{Path, Paths}
 
 class ModelExtensionsTest extends AnyFunSuiteLike {
 
-  test("buildTree should create correct result") {
-    val resourceRoot = getResourceRootPath
+  def withData(testCode: (Seq[Node], Path) => Any): Unit = {
+    val resourceRoot =
+      val url = getClass.getResource("/")
+      require(url != null, "Resource root path not found")
+      Paths.get(url.toURI)
     val inputPath = resourceRoot.resolve("input/directory-structure.csv").toString
-    val outputPath = resourceRoot.resolve("output/tree.txt").toString
-
     val (errors, nodes) = FileProcessor.processFile(inputPath)
     assert(errors.isEmpty)
+    testCode(nodes, resourceRoot)
+  }
 
-    val tree = nodes.buildTree()
+  test("buildTree should create correct result") {
+    withData { (nodes, resourceRoot) =>
+      val outputPath = resourceRoot.resolve("output/tree.txt").toString
 
-    val actual = tree.toDisplayString()
-    val expected = readFileContent(outputPath).replaceAll("\r\n", "\n").trim
+      val tree = nodes.buildTree()
 
-    actual shouldBe expected
+      val actual = tree.toDisplayString()
+      val expected = readFileContent(outputPath).replaceAll("\r\n", "\n").trim
+
+      actual shouldBe expected
+    }
   }
 
   test("filterBy 'top-secret' should create correct result") {
-    val resourceRoot = getResourceRootPath
-    val inputPath = resourceRoot.resolve("input/directory-structure.csv").toString
-    val outputPath = resourceRoot.resolve("output/top-secret.txt").toString
+    withData { (nodes, resourceRoot) =>
+      val outputPath = resourceRoot.resolve("output/top-secret.txt").toString
 
-    val (errors, nodes) = FileProcessor.processFile(inputPath)
-    assert(errors.isEmpty)
+      val result = nodes.filterBy(TopSecret)
 
-    val result = nodes.filterBy(TopSecret)
+      val actual = result.map(_.toDisplayString()).mkString("\n")
+      val expected = readFileContent(outputPath).replaceAll("\r\n", "\n").trim
 
-    val actual = result.map(_.toDisplayString()).mkString("\n")
-    val expected = readFileContent(outputPath).replaceAll("\r\n", "\n").trim
-
-    actual shouldBe expected
+      actual shouldBe expected
+    }
   }
 
   test("filterBy 'secret' should create correct result") {
-    val resourceRoot = getResourceRootPath
-    val inputPath = resourceRoot.resolve("input/directory-structure.csv").toString
-    val outputPath = resourceRoot.resolve("output/secret.txt").toString
+    withData { (nodes, resourceRoot) =>
+      val outputPath = resourceRoot.resolve("output/secret.txt").toString
 
-    val (errors, nodes) = FileProcessor.processFile(inputPath)
-    assert(errors.isEmpty)
+      val result = nodes.filterBy(Secret)
 
-    val result = nodes.filterBy(Secret)
+      val actual = result.map(_.toDisplayString()).mkString("\n")
+      val expected = readFileContent(outputPath).replaceAll("\r\n", "\n").trim
 
-    val actual = result.map(_.toDisplayString()).mkString("\n")
-    val expected = readFileContent(outputPath).replaceAll("\r\n", "\n").trim
-
-    actual shouldBe expected
+      actual shouldBe expected
+    }
   }
 
   test("filterBy 'secret' or 'top-secret' should create correct result") {
-    val resourceRoot = getResourceRootPath
-    val inputPath = resourceRoot.resolve("input/directory-structure.csv").toString
-    val outputPath = resourceRoot.resolve("output/secret-or-top-secret.txt").toString
+    withData { (nodes, resourceRoot) =>
+      val outputPath = resourceRoot.resolve("output/secret-or-top-secret.txt").toString
 
-    val (errors, nodes) = FileProcessor.processFile(inputPath)
-    assert(errors.isEmpty)
+      val result = nodes.filterBy(Secret, TopSecret)
 
-    val result = nodes.filterBy(Secret, TopSecret)
+      val actual = result.map(_.toDisplayString()).mkString("\n")
+      val expected = readFileContent(outputPath).replaceAll("\r\n", "\n").trim
 
-    val actual = result.map(_.toDisplayString()).mkString("\n")
-    val expected = readFileContent(outputPath).replaceAll("\r\n", "\n").trim
-
-    actual shouldBe expected
+      actual shouldBe expected
+    }
   }
 
   test("sizeBy 'public' should create correct result") {
-    val resourceRoot = getResourceRootPath
-    val inputPath = resourceRoot.resolve("input/directory-structure.csv").toString
+    withData { (nodes, _) =>
+      val result = nodes.sizeBy(Public)
 
-    val (errors, nodes) = FileProcessor.processFile(inputPath)
-    assert(errors.isEmpty)
-
-    val result = nodes.sizeBy(Public)
-
-    result shouldBe 120L
+      result shouldBe 120L
+    }
   }
 
   private def getResourceRootPath: Path =
